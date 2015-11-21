4c157aceb1c419fdc981f5a2ede2175d9cce29a5
Steven Prins <stepri@users.noreply.github.com>
2015-11-21 11:21:21 +0100

    Update hash.js

--- tasks/hash.js
+++ tasks/hash.js
@@ -55,7 +55,7 @@ module.exports = function(grunt) {
         grunt.file.copy(src, outputPath);
         grunt.log.writeln('Generated: ' + outputPath);
 
-        map[unixify(newFile)] = unixify(outKey);
+        map[unixify(basename + ext)] = unixify(outKey);
       });
     });
 
